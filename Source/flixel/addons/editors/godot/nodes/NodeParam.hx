4cae0b49f408a05fc2dc23a352f43ac4a7e8935c
Pekka Heikkinen <pekka.h.heikkinen@gmail.com>
2014-04-07 22:37:57 +0300

    Added some nodeparameters

---
+++ Source/flixel/addons/editors/godot/nodes/NodeParam.hx
@@ -0,0 +1,45 @@
+package flixel.addons.editors.godot.nodes;
+
+/**
+ * @author Pekka Heikkinen
+ */
+
+@:enum abstract NodeParam(String) from String to String
+{
+	var type = "type";
+	var name = "name";
+	
+	// Node
+	var process = "process/*";
+	var processPauseMode = "process/pause_mode";
+	var script = "script/script";
+	
+	// CanvasItem
+	var visibility = "visibility/*";
+	var visible = "visibility/visible";
+	var opacity = "visibility/opacity";
+	var selfOpacity = "visibility/self_opacity";
+	var behindParent = "visibility/behind_parent";
+	var onTop = "visibility/on_top";
+	var blendMode = "visibility/blend_mode";
+	
+	// Node2D
+	var transformation = "transform/*"
+	var position = "transform/pos";
+	var scale = "transform/scale";
+	var rotation = "transform/rot";
+	
+	// Sprite
+	var texture = "texture";
+	var centered = "centered";
+	var offset = "offset";
+	var flipH = "flip_h";
+	var flipV = "flip_v";
+	var vframes = "vframes";
+	var hframes = "hframes";
+	var frame = "frame";
+	var modulate = "modulate";
+	var region = "region";
+	var regionRect = "region_rect";
+	
+}
